2099d0ba595115a10b9a5d8f1cb7cfba6d5e270e
stefan022 <stefan022github@gmail.com>
2023-10-06 10:47:18 +0200

    devtools

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,14 @@ import './scss/main.scss'
 
 import { QueryClientProvider, } from '@tanstack/react-query'
 import { queryClient, root } from './constants'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 
 createRoot(root).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
             <App />
+            <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
     </StrictMode>
 )
